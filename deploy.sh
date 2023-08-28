2f021a301741445b422a2bf7cf5c6c78673d5e5d
Rick(Rugui) Chen <rickruguichen@google.com>
2023-08-28 15:58:42 -0400

    add web-client

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
+#1. Build llama 2 model container image:
+
+cd llama2-7b/llama2-7b
+gcloud build submit --region us-central1 --config cloudbuild.yaml
+
+cd ../../web-app
+gcloud build submit --region us-central1 --config cloudbuild.yaml
+
 # Set the Terraform variables
+
+cd ..
+
 export TF_VAR_project_id=rick-devops-01
 export TF_VAR_key_file=rick-devops-01-keys.json
 export TF_VAR_region=us-central1
 export TF_VAR_model=llama2-7b
 export TF_VAR_hf_api_token=$HF_API_TOKEN
+
 
 # Initialize Terraform
 terraform init
